ca3d3e0507c2c02d63db784c18c158d7696b80bd
mhorbatak <mhorbatak@districtmcreative.com>
2021-09-22 08:58:18 -0400

    updated index.js and readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# SENG560_Assgn_1
+# SENG 560 Programming Assignment 1 - Mike Horbatak
+
+## Introduction
+This is a NodeJS library that provides methods to compute basic arithmetic. These functions are as followed: 
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Square Root
+- Exponent
+- Conversion between the various types (octal, hexadecimal, integer, binary).
+
+## How to use this library
+
+To install/add this library to your NodeJS application, install the library using the following command.
+``` npm
+npm install seng560_assgn_1
+```
+
+Next, within your application, initialize the library with the following code.
+
+``` javascript
+const math = require('seng560_assgn_1');
+```
+Once initialized, the following functions are available:
+
+### Addition
+
+``` javascript
+var a = math.addNumbers(1, 3);
+// this will compute: 1 + 3 = 4
+console.log("The answer is: " + a);
+```
+
+### Subraction
+
+``` javascript
+var a = math.subractNumbers(4, 2);
+// this will compute 4 - 2 = 2
+console.log("The answer is: " + a);
+```
--- index.js
+++ index.js
@@ -1,51 +1,42 @@
 console.log("SENG 560 Programming Assignment 1");
 
 /* Unit test for addNumbers()
-var x = "number"
-var y = 1;
-var z = 2.2;
-var n = -2;
-
-var a = addNumbers(1, -2);
-console.log(a);
+For complete unit testing, see app.test.js 
 var a = addNumbers(1, -2);
 console.log(a);
 */
 
-
-
 const addNumbers = (a, b) => {
-
+    try {
     if (isNaN(a) || isNaN(b)) {
         return 'Please provide values and/or valid numbers';
       } else {
           return a + b;
       }
+    } catch (e) {
+        return "unexpected error occured";
+    }
 }
 
 exports.addNumbers = addNumbers;
-
-
-
 /* subtractNumbers() */
 
 /* Unit test for subractNumbers()
-var x = "number"
-var y = 1;
-var z = 2.2;
-var n = -2;
+For complete unit testing, see app.test.js 
 var a = subtractNumbers(1, -2);
 console.log(a);
-
 */
 
 const subtractNumbers = (a, b) =>{
-
+ try {
     if (isNaN(a) || isNaN(b)) {
         return 'Please provide values and/or valid numbers';
       } else {
           return a - b;
       }
+    } catch (e) {
+        return "unexpected error occured";
+    }
 }
 
 exports.subtractNumbers = subtractNumbers;
